58b33aff39673dd55a281a6e1a47ae694c360795
mai907 <maimohammed907@gmail.com>
2025-09-27 09:58:16 +0300

    Initialize Spring Boot project

---
+++ src/main/java/com/car/showrooms/CarShowroomsApiApplication.java
@@ -0,0 +1,13 @@
+package com.car.showrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShowroomsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShowroomsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/car/showrooms/CarShowroomsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.showrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShowroomsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
